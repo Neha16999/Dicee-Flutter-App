acee0b3bd069a54df4a4cb6b70acf5b3f51eb2b7
NehaRathi <neharathi1699@gmail.com>
2020-08-13 21:33:25 +0530

    Daily Learning

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Center(child: Text('Dicee')),
+          backgroundColor: Colors.red.shade700,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 5;
+  void changeDiceFace() {
+    setState(
+      () {
+        leftDiceNumber = Random().nextInt(6) + 1;
+        rightDiceNumber = Random().nextInt(6) + 1;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDiceFace();
+                print('left clicked');
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDiceFace();
+                print('right clicked');
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
